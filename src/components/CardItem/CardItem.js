7ffc454c44de92b42b5213afd957aa05fa642077
Frknzdmr1 <ozdemirf97@icloud.com>
2022-11-30 07:58:05 -0500

    CardItem V1.39

---
+++ src/components/CardItem/CardItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CardItem.css';
+
+const CardItem = () => {
+  return (
+    <div>CardItem</div>
+  )
+}
+
+export default CardItem
---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './CardList.css'
+
+const CardList = () => {
+  return (
+    <div className='CardList'>
+      <h1>Languages</h1>
+      <div className='cards'>
+        <p>Card 1</p>
+        <p>Card 2</p>
+        <p>Card 3</p>
+      </div>
+    </div>
+  )
+}
+
+export default CardList
